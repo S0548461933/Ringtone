3ebed66defea19a8d90a9d1425989772d492612c
S0548461933 <sarit61933@gmail.com>
2024-05-23 12:40:01 +0300

    fixed

---
+++ client/ringtones/src/app/holiday-block/holiday-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-holiday-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './holiday-block.component.html',
+  styleUrl: './holiday-block.component.scss'
+})
+export class HolidayBlockComponent {
+
+}
---
+++ client/ringtones/src/app/song-list/song-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-list.component.html',
+  styleUrl: './song-list.component.scss'
+})
+export class SongListComponent {
+
+}
